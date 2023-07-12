d534a9d44ab55c512f4631e523638de783d7e89f
Ayush Varshney <ayush.varshnay_iot18@gla.ac.in>
2023-07-13 00:40:29 +0530

    largest subaaray k sum

---
+++ Array_IV/Largest Subarray with K sum.py
@@ -0,0 +1,16 @@
+def LongestSubsetWithZeroSum(arr):
+    n=len(arr)
+    A=arr
+    mpp = {}
+    maxi = 0
+    sum = 0
+    for i in range(n):
+        sum += A[i]
+        if sum == 0:
+            maxi = i + 1
+        else:
+            if sum in mpp:
+                maxi = max(maxi, i - mpp[sum])
+            else:
+                mpp[sum] = i
+    return maxi
