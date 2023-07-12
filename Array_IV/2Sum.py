334581b3a7702d3553b34c0dacbbfdc25710acb6
Ayush Varshney <ayush.varshnay_iot18@gla.ac.in>
2023-07-13 00:39:40 +0530

    2Sum

--- Array_IV/2Sum.py
+++ Array_IV/2Sum.py
@@ -1,3 +1,4 @@
+from collections import *
 
 def twoSum(nums, target):
     h={}
@@ -8,5 +9,21 @@ def twoSum(nums, target):
         else:
             return [h[r],i]
 
+# Coding Ninja
+def pairSum(arr, s):
+    # Write your code here.
+    h=defaultdict(int)
+    a=[]
+    for i in arr:
+        c=h[s-i]
+        p=[i,s-i]
+        while c>0:
+            a.append(sorted(p))
+            c-=1
+        h[i]+=1
+
+    return sorted(a)
+
 if __name__=="__main__":
     print(twoSum(nums = [2,7,11,15], target = 9))
+    print(pairSum([2,7,11,15],9))
